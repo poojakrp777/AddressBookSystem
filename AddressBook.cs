2203ac30a9c7fdb14af9b6d9db676e4d9b3d71e4
poojakrp777 <poojakrp777@gmail.com>
2023-02-01 21:37:43 +0530

    [POOJA] Address Book system Adding New Contact

---
+++ AddressBook.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    internal class AddressBook
+    {
+        List<Contact> contacts = new List<Contact>();
+        public void addContact(string firstName, string email, string lastName, string phoneNumber, string address, string zip, string city, string state)
+        {
+            contacts.Add(new Contact()
+            {
+                firstName = firstName,
+                lastName = lastName,
+                email = email,
+                phoneNo = phoneNumber,
+                address = address,
+                zip = zip,
+                city = city,
+                state = state,
+            });
+            Console.WriteLine($"Contact of {firstName}{lastName} has been added");
+        }
+    }
+}
